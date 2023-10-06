34b5a85c72f37ab7afabe7ecf0f47fdb9821e8c5
Daniel M. Bomfim <daniellsmv@hotmail.com>
2023-10-06 20:57:36 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 import router from './routes';
 
 const server: Application = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use(cors());
 server.use(router);
 
 export default server;
